c710bf2e8c4469d4e81f0a210343c0198319a44c
Iván Alonso <iarjona@qdqmedia.com>
2022-05-23 16:35:42 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,7 @@ class Dev(Configuration):
         'rest_framework.authtoken',
         'drf_yasg',
         'django_filters',
+        'versatileimagefield',
         'blango_auth',
         'blog',
         'allauth',
@@ -240,6 +241,9 @@ class Dev(Configuration):
         "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
         "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
+
+    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_URL = "/media/"
 
 
 class Prod(Dev):
