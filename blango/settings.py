f858febd88fa323f367f0efaa2d5cf305ab8da31
Iván Alonso <iarjona@qdqmedia.com>
2022-05-23 16:24:26 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -2,6 +2,7 @@ import os
 import dj_database_url
 from pathlib import Path
 from configurations import Configuration, values
+from datetime import timedelta
 
 
 class Dev(Configuration):
@@ -206,6 +207,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
         "DEFAULT_THROTTLE_CLASSES": [
             "blog.api.throttling.AnonSustainedThrottle",
@@ -232,6 +234,11 @@ class Dev(Configuration):
             "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
             "Basic": {"type": "basic"},
         }
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
 
